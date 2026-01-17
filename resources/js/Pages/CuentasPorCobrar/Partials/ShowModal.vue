4569b055271861644b4570a0c6af2de35e6f4ac5
Dev Local <dev@local>
2026-01-17 16:05:25 -0700

    Fix: Evitar error replace en metodo_pago undefined

--- resources/js/Pages/CuentasPorCobrar/Partials/ShowModal.vue
+++ resources/js/Pages/CuentasPorCobrar/Partials/ShowModal.vue
@@ -146,7 +146,7 @@
                                             <svg v-else class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" /></svg>
                                         </div>
                                         <div>
-                                            <p class="text-sm font-black text-gray-900 capitalize">{{ pago.metodo_pago.replace('_', ' ') }}</p>
+                                            <p class="text-sm font-black text-gray-900 capitalize">{{ (pago.metodo_pago || 'N/A').replace('_', ' ') }}</p>
                                             <p class="text-[10px] text-gray-400 font-bold uppercase mt-0.5 tracking-wider">
                                                 {{ formatDateShort(pago.fecha_entrega) }} 
                                                 <span v-if="pago.cuenta_bancaria">• {{ pago.cuenta_bancaria.banco }}</span>
